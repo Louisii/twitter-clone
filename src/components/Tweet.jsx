375b4b41eed9f8974739028ae94284dddf4a184f
Louisi <louisimtd@outlook.com>
2023-04-03 22:50:01 -0300

    design tweet ok

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,16 +1,18 @@
 import ProfilePitcure from "./ProfilePicture"
+import { FaRegComment, FaRetweet, FaRegHeart, FaRegChartBar, FaShareSquare } from "react-icons/fa";
 
 const Tweet = (props) => {
     return (
-        <div className="md:max-w-xl">
+        <div className="grid grid-cols-10 max-w-xl border dark:border-gray-800 p-3">
 
-            <div className="inline-block ">
+            <div className="col-span-1 ">
                 <ProfilePitcure />
             </div>
 
-            <div className="align-top inline-block pl-4 pt-1">
+            <div className="col-span-9 pl-4 pt-1">
 
                 <div className="grid grid-cols-2">
+
                     <div className=" text-left">
 
                         <h2 className="dark:text-white font-bold inline-block mr-2 hover:underline text-left">
@@ -22,16 +24,24 @@ const Tweet = (props) => {
                         </h3>
                     </div>
 
-                    <div className=" text-right">
-                        <h3 className="dark:text-gray-400 ml-36 hover:text-blue-400 hover:bg-blue-950 rounded-full px-1">
+                    <div className="w-full text-right">
+                        <h3 className="dark:text-gray-400 hover:text-blue-400 rounded-full px-1 pb-1">
                             •••
                         </h3>
 
                     </div>
+
                 </div>
 
-                <div className="dark:text-white">
-                    <p>{props.content}</p>
+                <div className="dark:text-white m-0 w-full text-left pr-4">
+                    <p>{props.content}</p> 
+                </div>
+                <div className="grid grid-cols-5 dark:text-gray-400 mt-4 text-center">
+                    <FaRegComment className="hover:text-blue-400 hover:bg-blue-950 rounded-full"/> 
+                    <FaRetweet className="dark:text-gray-600 text-xl"/> 
+                    <FaRegHeart className="hover:text-pink-700"/>
+                    <FaRegChartBar className="hover:text-blue-400 hover:bg-blue-950 rounded-full"/>
+                    <FaShareSquare className="hover:text-blue-400 hover:bg-blue-950 rounded-full"/>
                 </div>
 
             </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,11 @@ const Home = (props) => {
 
     return(
         <>
-        <Tweet userName="Lou" user="@louisimtd" postingTime="6h" content="estou pronto para essa semana de 3 dias úteis"/>
+
+            <Tweet userName="Lou" user="@louisimtd" postingTime="6h" content="estou pronto para essa semana de 3 dias úteis"/>
+            <Tweet userName="Lou" user="@louisimtd" postingTime="8h" content="ha 5 minutos faltavam 9 minutos pro ônibus passar e agora, depois de esperar 5 minutos, ele está a 12 min de distância 🥰🥰 amo o floripanopinto"/>
+            <Tweet userName="Lou" user="@louisimtd" postingTime="6h" content="moça não me julgue eu n consigo fazer o mov abdominal mais amplo sem me cagar 🤭"/>
+            <Tweet userName="Lou" user="@louisimtd" postingTime="6h" content="dai o cara parece o brad pitt pessoalmente e quando vc entra no insta so tem foto horrivel igual de pai"/>
         </>
     )
 }
