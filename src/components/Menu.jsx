edc19802deaa1b8e768cd0062b6d14d9ead41b62
Louisi <louisimtd@outlook.com>
2023-04-06 21:47:24 -0300

    menu dinamico com map

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,42 +2,55 @@ import { FaTwitter, FaHome, FaHashtag, FaBell, FaRegEnvelope, FaBookmark, FaTwit
 
 const Menu = () => {
 
-    return(
+    const menu = [
+        {
+            icon: <FaHashtag className="mt-0.5 text-2xl" />,
+            title: "Explore"
+        },
+        {
+            icon: <FaBell className="mt-0.5 text-2xl" />,
+            title: "Notifications"
+        },
+        {
+            icon: <FaRegEnvelope className="mt-0.5 text-2xl" />,
+            title: "Message"
+        },
+        {
+            icon: <FaBookmark className="mt-0.5 text-2xl" />,
+            title: "Bookmarks"
+        },
+        {
+            icon: <FaTwitterSquare className="mt-0.5 text-2xl" />,
+            title: "Twitter Blue"
+        },
+        {
+            icon: <FaUserAlt className="mt-0.5 text-2xl" />,
+            title: "Profile"
+        },
+        {
+            icon: <FaEllipsisH className="mt-0.5 text-2xl" />,
+            title: "More"
+        },
+    ]
+
+    return (
         <>
             <div className="dark:text-white">
-            <FaTwitter className="mt-0.5 text-3xl mb-8"/>
+                <FaTwitter className="mt-0.5 text-3xl mb-8" />
                 <div className="grid grid-cols-6 mb-8">
-                    <FaHome className="mt-0.5 text-2xl"/>
+                    <FaHome className="mt-0.5 text-2xl" />
                     <h1 className="col-span-5 dark:text-white font-extrabold text-xl text-left ml-3">Home</h1>
                 </div>
-                <div className="grid grid-cols-6 mb-8">
-                    <FaHashtag className="mt-0.5 text-2xl"/>
-                    <h1 className="col-span-5 dark:text-white font-semibold text-xl text-left ml-3">Explore</h1>
-                </div>
-                <div className="grid grid-cols-6 mb-8">
-                    <FaBell className="mt-0.5 text-2xl"/>
-                    <h1 className="col-span-5 dark:text-white font-semibold text-xl text-left ml-3">Notifications</h1>
-                </div>
-                <div className="grid grid-cols-6 mb-8">
-                    <FaRegEnvelope className="mt-0.5 text-2xl"/>
-                    <h1 className="col-span-5 dark:text-white font-semibold text-xl text-left ml-3">Message</h1>
-                </div>
-                <div className="grid grid-cols-6 mb-8">
-                    <FaBookmark className="mt-0.5 text-2xl"/>
-                    <h1 className="col-span-5 dark:text-white font-semibold text-xl text-left ml-3">Bookmarks</h1>
-                </div>
-                <div className="grid grid-cols-6 mb-8">
-                    <FaTwitterSquare className="mt-0.5 text-2xl"/>
-                    <h1 className="col-span-5 dark:text-white font-semibold text-xl text-left ml-3">Twitter Blue</h1>
-                </div>
-                <div className="grid grid-cols-6 mb-8">
-                    <FaUserAlt className="mt-0.5 text-2xl"/>
-                    <h1 className="col-span-5 dark:text-white font-semibold text-xl text-left ml-3">Profile</h1>
-                </div>
-                <div className="grid grid-cols-6 mb-8">
-                    <FaEllipsisH className="mt-0.5 text-2xl"/>
-                    <h1 className="col-span-5 dark:text-white font-semibold text-xl text-left ml-3">More</h1>
-                </div>
+
+                {
+                    menu.map((menuItem) => (
+                        <div className="grid grid-cols-6 mb-8">
+                            {menuItem.icon}
+                            <h1 className="col-span-5 dark:text-white font-semibold text-xl text-left ml-3">{menuItem.title}</h1>
+                        </div>
+                    ))
+                }
+
             </div>
         </>
     )
